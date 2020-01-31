0545a05fc1c3d36bac68d3e71f2c0a0c0f6677b9
Elsie Ashworth <44971680+eashworth@users.noreply.github.com>
2020-01-31 20:43:58 +0000

    Add elsif and third string to pass third test

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,9 +1,11 @@
 class Bottles
   def verse(num_bottles)
     if num_bottles == 99
-"99 bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer on the wall.\n"
+      "99 bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer on the wall.\n"
+    elsif num_bottles == 89
+      "89 bottles of beer on the wall, 89 bottles of beer.\nTake one down and pass it around, 88 bottles of beer on the wall.\n"
     else
-"89 bottles of beer on the wall, 89 bottles of beer.\nTake one down and pass it around, 88 bottles of beer on the wall.\n"
+      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
     end
   end
 end
