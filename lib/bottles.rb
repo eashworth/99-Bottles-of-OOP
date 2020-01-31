1407b3e6da52bb5636c7669bd76f33bb65d20acd
Elsie Ashworth <44971680+eashworth@users.noreply.github.com>
2020-01-31 20:14:50 +0000

    Add if/else with second string to pass second test.

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,5 +1,9 @@
 class Bottles
   def verse(num_bottles)
+    if num_verses == 99
 "99 bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer on the wall.\n"
+    else
+"89 bottles of beer on the wall, 89 bottles of beer.\nTake one down and pass it around, 88 bottles of beer on the wall.\n"
+    end
   end
 end
