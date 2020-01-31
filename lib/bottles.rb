a936d2f665aafbbfd35f51139155c8ac563c9a11
Elsie Ashworth <44971680+eashworth@users.noreply.github.com>
2020-01-31 20:17:44 +0000

    Amend variable name num_verses to num_bottles

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,6 +1,6 @@
 class Bottles
   def verse(num_bottles)
-    if num_verses == 99
+    if num_bottles == 99
 "99 bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer on the wall.\n"
     else
 "89 bottles of beer on the wall, 89 bottles of beer.\nTake one down and pass it around, 88 bottles of beer on the wall.\n"
