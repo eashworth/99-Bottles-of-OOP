8f450971a173ad9bd1c53c3151680558cb662d59
Elsie Ashworth <44971680+eashworth@users.noreply.github.com>
2020-01-31 21:10:50 +0000

    Refactor: use variable bobs = 'bottles of beer'

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,15 +1,16 @@
 class Bottles
   def verse(num_bottles)
-    bob = "bottles of beer"
+    bobs = "bottles of beer"
+    bob = "bottle of beer"
     otw = "on the wall"
     take = "Take one down and pass it around"
 
     if num_bottles > 2
-      "#{num_bottles} #{bob} #{otw}, #{num_bottles} #{bob}.\n#{take}, #{num_bottles - 1} #{bob} #{otw}.\n"
+      "#{num_bottles} #{bobs} #{otw}, #{num_bottles} #{bobs}.\n#{take}, #{num_bottles - 1} #{bobs} #{otw}.\n"
     elsif num_bottles == 2
-      "#{num_bottles} #{bob} #{otw}, #{num_bottles} #{bob}.\n#{take}, #{num_bottles - 1} bottle of beer #{otw}.\n"
+      "#{num_bottles} #{bobs} #{otw}, #{num_bottles} #{bobs}.\n#{take}, #{num_bottles - 1} #{bob} #{otw}.\n"
     else
-      "#{num_bottles} bottle of beer #{otw}, #{num_bottles} bottle of beer.\nTake it down and pass it around, no more bottles of beer #{otw}.\n"
+      "#{num_bottles} #{bob} #{otw}, #{num_bottles} bottle of beer.\nTake it down and pass it around, no more #{bobs} #{otw}.\n"
     end
   end
 end
